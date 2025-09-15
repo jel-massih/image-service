4d78c9ff53629eb73f8493b036c05ce108cdefa7
Jason El-Massih <jel-massih@hotmail.com>
2025-09-15 15:43:54 -0400

    Add functions

--- main.ts
+++ main.ts
@@ -9,3 +9,68 @@ export async function blur(
         img.blur(blurRadius, blurSigma);
     });
 }
+
+export async function resize(
+    url: string,
+    width: number,
+    height: number,
+): Promise<Uint8Array> {
+    return handleImage(url, (img) => {
+        img.resize(width, height);
+    });
+}
+
+export async function crop(
+    url: string,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+): Promise<Uint8Array> {
+    return handleImage(url, (img) => {
+        img.crop(width, height, x, y);
+    });
+}
+
+export async function rotate(
+    url: string,
+    angle: number,
+) {
+    return handleImage(url, (img) => {
+        img.rotate(angle);
+    });
+}
+
+export async function flip(url: string): Promise<Uint8Array> {
+    return handleImage(url, (img) => {
+        img.flip();
+    });
+}
+
+export async function flop(url: string): Promise<Uint8Array> {
+    return handleImage(url, (img) => {
+        img.flop();
+    });
+}
+
+export async function grayscale(url: string): Promise<Uint8Array> {
+    return handleImage(url, (img) => {
+        img.grayscale();
+    });
+}
+
+export async function invert(url: string): Promise<Uint8Array> {
+    return handleImage(url, (img) => {
+        img.negate();
+    });
+}
+
+export async function sharpen(
+    url: string,
+    radius = 0,
+    sigma = 1,
+): Promise<Uint8Array> {
+    return handleImage(url, (img) => {
+        img.sharpen(radius, sigma);
+    });
+}
