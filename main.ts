5d8ae2523705bb3abdf06dfa63acdfe6d8e0b3a6
Jason El-Massih <jel-massih@hotmail.com>
2026-01-21 17:06:55 -0500

    Fix comment typo in main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { Gravity, MagickFormat, MagickGeometry } from '@imagemagick/magick-wasm';
 import { handleImage } from './imagick.ts';
-//edit
+//e
 export const VERSION = '1.0';
 
 export async function blur(
